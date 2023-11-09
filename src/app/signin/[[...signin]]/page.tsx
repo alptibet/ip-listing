4871d590f2bad212f9a4ad0ac3ef82053da4fcec
alptibet <a.tibet@yandex.ru>
2023-11-09 19:28:14 +0300

    done delete project

--- src/app/signin/[[...signin]]/page.tsx
+++ src/app/signin/[[...signin]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs';
 export default function Page() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <SignIn afterSignInUrl={'/'} />
+      <SignIn afterSignInUrl={'/dashboard'} />
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,7 +17,6 @@ export default async function Header() {
   return (
     <div className="flex items-center justify-between mx-2 my-2">
       <div className="flex items-center gap-5">
-        <ButtonLink label="HOME" url="/" />
         {user && <ProjectSwitcher />}
       </div>
 
