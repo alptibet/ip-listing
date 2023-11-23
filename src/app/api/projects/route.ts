b5547c37b68b6c1b00d7ed6044acd8961f6b526e
alptibet <a.tibet@yandex.ru>
2023-11-23 23:11:16 +0300

    project routes with drizzle

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { dbClient } from '@/db/db';
 import { drizzle } from 'drizzle-orm/postgres-js';
+import { projects } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 
 export async function GET() {
   try {
@@ -16,38 +18,34 @@ export async function GET() {
   }
 }
 
-// export async function POST(req: NextRequest) {
-//   const body = await req.json();
-//   try {
-//     const newProject = await prisma.project.create({
-//       data: { name: body.name },
-//     });
-//     return NextResponse.json(newProject, { status: 201 });
-//   } catch (error: Prisma.PrismaClientKnownRequestError | any) {
-//     return NextResponse.json(
-//       {
-//         message: error.message,
-//       },
-//       { status: 400 }
-//     );
-//   }
-// }
-//
-// export async function DELETE(req: NextRequest) {
-//   const body = await req.json();
-//   try {
-//     const deleteDevice = await prisma.project.delete({
-//       where: {
-//         name: body,
-//       },
-//     });
-//     return NextResponse.json(deleteDevice, { status: 201 });
-//   } catch (error: Prisma.PrismaClientKnownRequestError | any) {
-//     return NextResponse.json(
-//       {
-//         message: error.message,
-//       },
-//       { status: 400 }
-//     );
-//   }
-// }
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const newProject = await dbClient.insert(projects).values(body);
+    return NextResponse.json(newProject, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      { status: 400 }
+    );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const deleteDevice = await dbClient
+      .delete(projects)
+      .where(eq(projects.name, body));
+    return NextResponse.json(deleteDevice, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      { status: 400 }
+    );
+  }
+}
