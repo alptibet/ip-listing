d6413a0480da55e3cb6b858dcf9b3a0642f580bb
alptibet <a.tibet@yandex.ru>
2023-11-26 13:40:14 +0300

    env correction

--- src/app/api/devicesApi.jsx
+++ src/app/api/devicesApi.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const devicesApi = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? process.env.DB_URL
+      ? process.env.NEXT_PUBLIC_URL
       : 'http://localhost:3000',
 });
 
